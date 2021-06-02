eaeef748763fc27ff949312be49f07bc941e9cc2
WilliamWang90923 <wangxuefeng90923@yahoo.co.jp>
2021-06-02 15:56:13 +0900

    basic GUI complete

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+        } 
+    }
+  },
+  configureWebpack: (config) => {
+    if (process.env.npm_config_report) {
+      const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+      config.plugins.push(new BundleAnalyzerPlugin())
+    }
+  },
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/music-next/' : '/'
+}
